0bacfaa66119a415f193a28d5cb765c857cba464
trinkner <42588724+duneswake@users.noreply.github.com>
2021-01-25 20:12:22 -0700

    Delete src/main directory

--- src/main/pool/Task.java
+++
@@ -1,8 +0,0 @@
-package main.pool;
-
-import main.model.Model;
-
-public interface Task {
-    Model[] parent = new Model[0];
-    void run();
-}
--- src/main/tasks/Task.java
+++
@@ -1,8 +0,0 @@
-package main.tasks;
-
-import main.form.Form;
-
-public interface Task {
-    Form[] parent = new Form[0];
-    void run();
-}
